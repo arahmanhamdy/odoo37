109f4c6e971cefbc101c61607b477107818e359c
seifElislam <seif.eleslam1990@gmail.com>
2017-06-03 23:45:45 +0200

    add actions to menus

--- custom_employee/__manifest__.py
+++ custom_employee/__manifest__.py
@@ -7,6 +7,7 @@
         'views/relatives.xml',
         'views/certificates.xml',
         'views/certificate_tab.xml',
-        'data/relation_types.xml'
+        'data/relation_types.xml',
+        'views/training_menus.xml'
     ],
 }
