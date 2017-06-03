f97aeb7fc16ada8b06cb1b82a64cc4c9591d7a4a
seifElislam <seif.eleslam1990@gmail.com>
2017-06-03 23:32:43 +0200

    add training menus

--- custom_employee/models/training_provider.py
+++ custom_employee/models/training_provider.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class TrainingProvider(models.Model):
     _name = 'custom_employee.training_provider'
     name = fields.Char()
-    adress = fields.Text()
+    address = fields.Text()
     phone_number = fields.Char()
